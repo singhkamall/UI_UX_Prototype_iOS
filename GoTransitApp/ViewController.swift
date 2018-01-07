da1ad410a57c7ee1f47140fef523ccc727d560d7
Kamalpreet Singh <kamalpreetsingh@kamalpreets-mac.local>
2018-01-07 11:34:33 -0800

    Project Created

--- GoTransitApp/ViewController.swift
+++ GoTransitApp/ViewController.swift
@@ -1,25 +1,9 @@
-//
-//  ViewController.swift
-//  GoTransitApp
-//
-//  Created by Kamalpreet Singh on 2018-01-07.
-//  Copyright © 2018 GoTransit. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
-
